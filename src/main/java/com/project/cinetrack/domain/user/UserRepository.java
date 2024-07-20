8f97a011230d8bb1d257467afbdb2311e5a94f5b
Otaviano Cruz Gouveia de Arruda Neto <0t4v14n0@gmail.com>
2024-07-20 02:21:51 -0300

    update query

--- src/main/java/com/project/cinetrack/domain/user/UserRepository.java
+++ src/main/java/com/project/cinetrack/domain/user/UserRepository.java
@@ -8,7 +8,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	@Query("SELECT new com.project.cinetrack.domain.user.DataDeteilsUser(u.id, u.name, u.email, u.bio, u.avatar_url) FROM User u WHERE u.name = ?1")
+	@Query("SELECT new com.project.cinetrack.domain.user.DataDeteilsUser(u.id,"
+			+ "u.name,"
+			+ "u.email,"
+			+ "u.bio,"
+			+ "u.avatar_url)"
+			+ "FROM User u WHERE u.name = ?1")
 	DataDeteilsUser findDataDetailsByName(String name);
 	
 	UserDetails findByEmail(String Email);
